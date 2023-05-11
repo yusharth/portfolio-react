43b81e3d61222c666c288d5b9041acc90b696557
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-05-11 23:23:19 +0530

    resolved div inside canvas error

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <Html>
+      <div>Loading...</div>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
