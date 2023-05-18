4725d1fb4cf81a2ee698e80e62687f1fe60517e3
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-05-18 09:59:05 +0530

    resolved Icosahedron error using extend

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,30 +1,43 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import
-{
-  Decal,Float,OrbitControls,Preload,useTexture
-}
-from '@react-three/drei';
-import CanvasLoader from '../Loader';
-import { IcosahedronGeometry } from 'three';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import { extend } from "@react-three/fiber";
+import CanvasLoader from "../Loader";
+import { IcosahedronGeometry } from "three";
+extend(IcosahedronGeometry);
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
   return (
     <Float speed={1.75}>
-      <ambientLight intensity={0.25}/>
-      <directionalLight position={[0,0,0.05]}/>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow={2.75}>
-        <IcosahedronGeometry args={[1,1]} />
-        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} map={decal} />
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
       </mesh>
     </Float>
-  )
-}
+  );
+};
 
-const BallCanvas = ({icon}) =>{
-  return(
+const BallCanvas = ({ icon }) => {
+  return (
     <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
@@ -32,7 +45,7 @@ const BallCanvas = ({icon}) =>{
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default BallCanvas
+export default BallCanvas;
